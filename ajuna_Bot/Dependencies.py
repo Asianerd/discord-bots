5ad301dbc0d25ac1779ea6e4ec06d09c2265bba2
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2021-11-21 17:25:37 +0800

    Moved delayed_delete and dispose_message to Dependencies

--- ajuna_Bot/Dependencies.py
+++ ajuna_Bot/Dependencies.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import pickle
+from enum import Enum
 
 command_prefix = "\\"
 author_id = 517998886141558786
@@ -10,4 +11,25 @@ authorized_users = [517998886141558786, 859005581414236160]
 emojis = []
 disposable_messages = []
 
-reactions = ["\N{WASTEBASKET}"]
+
+class ReactionType(Enum):
+    wastebasket = 0
+    no1 = 1
+    no2 = 2
+    no3 = 3
+    no4 = 4
+    no5 = 5
+
+
+reactions = {
+    ReactionType.wastebasket: "\N{WASTEBASKET}",
+}
+
+
+async def delayed_delete(message, delay=3):
+    await message.delete(delay=delay)
+
+
+async def dispose_message(ctx):
+    disposable_messages.append(ctx)
+    await ctx.add_reaction(reactions[ReactionType.wastebasket])
