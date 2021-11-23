1fdf2d2ff445d55c82de4ea4bbf70b5b7b630f03
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2021-11-23 10:06:41 +0800

    Shop system implemented

--- ajuna_Bot/Economy/Economy.py
+++ ajuna_Bot/Economy/Economy.py
@@ -1,6 +1,8 @@
 import sys
+import math
 from . import User
 from . import Task
+from . import Item
 import Formatting
 import Dependencies
 import discord
@@ -18,6 +20,15 @@ rewards, reputation
 - Tasks         (done)
 - Reputation
 """
+
+
+def select_next_few(iterable, amount, start):
+    final = []
+    selection_range = range(start, start + amount)
+    for x in enumerate(iterable):
+        if x[0] in selection_range:
+            final.append(x[1])
+    return final
 
 
 async def on_message(client, message):
@@ -39,6 +50,9 @@ async def on_message(client, message):
 async def on_reaction_add(client, reaction, _user):
     if _user.bot:
         return
+
+    User.User.update_user_data(_user.id, _user.name)
+
     await User.User.update_progress(
         reaction.message.channel,
         User.User.users[_user.id],
@@ -120,3 +134,70 @@ def init(client):
         Task.Task.task_log = []
         Task.Task.update_task_log()
         await task_select(ctx)
+    
+    @client.command()
+    async def shop(ctx, args="1"):
+        await ctx.message.delete()
+        try:
+            page = int(args) - 1
+        except ValueError:
+            await ctx.send("Invalid input entered.")
+            return
+
+        bs = '\\'
+        item_per_page = 5
+        page_items = select_next_few(list(Item.Item.item_data.keys()), item_per_page, page * item_per_page)
+        if len(page_items) <= 0:
+            await (await ctx.send(embed=discord.Embed(
+                title="Out of range!",
+                color=Formatting.colour()
+            ))).delete(delay=3)
+            return
+
+        _description = ''
+        for x in page_items:
+            _item = Item.Item.item_data[x]
+            _description += f"**{_item['emoji']} {_item['name'].replace('_',f'{bs}_')}**  - {_item['value']}c\n"\
+                            f"- _{str(_item['description']).replace('_',f'{bs}_')}_\n\n"
+        final = discord.Embed(
+            title="Shop",
+            description=_description,
+            color=Formatting.colour()
+        )
+        final.set_footer(text=f"Page {page + 1} of {int(math.ceil(len(Item.Item.item_data)/item_per_page))}")
+        await Dependencies.dispose_message(await ctx.send(embed=final))
+
+    @client.command()
+    async def buy(ctx, *args):
+        await ctx.message.delete()
+        try:
+            if len(args) >= 2:
+                amount = int(args[1])
+            else:
+                amount = 1
+            if amount <= 0:
+                await (await ctx.send(embed=discord.Embed(title="bruh", color=Formatting.colour()))).delete(delay=3)
+                return
+        except:
+            await ctx.send(embed=discord.Embed(
+                title="Invalid input entered. Please re-enter.",
+                color=Formatting.colour()
+            ))
+            return
+        key_names = [(str(x[1]).lower().split('.')[-1], x[0]) for x in enumerate(list(Item.Item.item_data.keys()))]
+        if args[0].lower() in [x[0] for x in key_names]:
+            _item_str = list(Item.Item.item_data.keys())[[x[1] for x in key_names if x[0] == args[0].lower()][0]]
+            _item_type = [x for x in Item.ItemType if str(_item_str).split('.')[-1] == x.name][0]
+            _item = Item.Item.item_data[str(_item_type)]
+            _user = User.User.users[ctx.message.author.id]
+            if _user.coins >= (_item['value'] * amount):
+                await ctx.send(embed=discord.Embed(title="Purchase succesful!", description=f"{_item['emoji']} {amount} {_item['name']}{('s' if amount > 1 else '')}", color=Formatting.colour()))
+                _user.inventory.add(_item_type, amount)
+                _user.coins -= (_item['value'] * amount)
+            else:
+                await ctx.send(embed=discord.Embed(title="You don't have enough coins.", color=Formatting.colour()))
+        else:
+            await (await ctx.send(embed=discord.Embed(
+                title="Item doesn't exist. Please check the shop for the item names.",
+                color=Formatting.colour()
+            ))).delete(delay=3)
